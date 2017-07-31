4333ee9df3cda4855557645ecfefa98c21857b60
Haneen Abu-Khater <abukhater.haneen@gmail.com>
2017-07-30 18:06:23 -0700

    add dry code to ask for all meals

---
+++ src/main/java/MealTime.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MealTime {
+
+    public static void main(String[] args) {
+
+            askWhatYouAteFor("breakfast");
+            askWhatYouAteFor("lunch");
+            askWhatYouAteFor("dinner");
+    }
+    public static void askWhatYouAteFor(String meal){
+        try {
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("What did you eat for " + meal + "?");
+            String yourMeal = bufferedReader.readLine();
+            System.out.println("You had " + yourMeal + " for " + meal + ".");
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
